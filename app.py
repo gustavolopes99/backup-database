66cfd433feff1bc814b29143184555ef983d4e25
Gustavo Lopes <glopoh@gmail.com>
2023-02-15 14:08:53 -0300

    Added some functions

--- app.py
+++ app.py
@@ -1,34 +1,72 @@
+import tkinter as tk
 from tkinter import *
 from tkinter import filedialog
 from tkinter.messagebox import showinfo
+import subprocess
 import zipfile
 
 # Base root para criação da janela principal
-root = Tk()
+
+root = tk.Tk()
 root.iconbitmap("paper.ico")
 root.title("Backup Ecodados")
-root.geometry(f'{300}x{200}')
-
+root.resizable(False, False)
 
 # Functions
 
-def openFile():
+
+def openFileEco():
     # Abrir uma caixa de diálogo para selecionar o dir
     root.filename = filedialog.askopenfilename(initialdir='C:/ecosis/dados', title='Selecione um arquivo', filetypes=(
         ('Base de dados', '*.eco'), ('All files', '*.*')))
     # my_label definirá o diretório a ser exibido na tela
-    my_label = Label(root, text=root.filename).pack()
-    print(root.filename)
+    my_label = Label(root, text=(
+        f'Base de dados: {root.filename}')).pack()
+    remSubstr = root.filename
+    addSubstr = remSubstr[:-3]
+    gbkext = 'GBK'
+    root.result = addSubstr + gbkext
 
 
 def zipFile():
-    inzip = zipfile.ZipFile('zipado.zip', 'w')
+    inzip = zipfile.ZipFile('{root.filename}.7z', 'w')
     inzip.write(root.filename)
     inzip.close()
 
 
-# Buttons
-my_btn = Button(root, text="Informar base de dados", command=openFile).pack()
-inzip = Button(root, text="Backup", command=zipFile).pack()
+def openFileFB():
+    global getDirectory
+    root.foldername = filedialog.askdirectory(
+        initialdir='C:', title='Selecione um arquivo')
+    binlabel = Label(root, text=(
+        f'Bin firebird: {root.foldername}')).pack()
+    getDirectory = root.foldername
+
+
+def findPort():
+    sendPort = entinput.get()
+    Label(root, text=(f'Porta {sendPort} enviada')).pack()
+
+
+def Gbk():
+    return_code = subprocess.call(
+        f'cd {openFileFB()} gbak -g -b -l -v {root.filename} {getDirectory} -user sysdba -pass masterkey', shell=True)
+
+    # Buttons Main Window
+
+
+baseEco = Button(root, text="Informar base de dados",
+                 command=openFileEco, height=0, width=18).pack()
+windFB = Button(root, text="FB Bin", command=openFileFB,
+                height=0, width=18).pack()
+enterPort = Label(root, text="Informe a porta").pack()
+digitPort = tk.Canvas(root, width=400, height=150, relief='raised')
+digitPort.pack()
+entinput = tk.Entry(root, width=8, justify='center')
+confirmarPorta = Button(root, height=0, width=18,
+                        text="Enviar Porta", command=findPort).pack()
+digitPort.create_window(200, 10, window=entinput)
+inzip = Button(root, text="Backup", command=Gbk,
+               height=0, width=18).pack()
 
 root.mainloop()
---
+++ s.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+
+root= tk.Tk()
+
+canvas1 = tk.Canvas(root, width=400, height=300, relief='raised')
+canvas1.pack()
+
+entry1 = tk.Entry(root) 
+canvas1.create_window(200, 140, window=entry1)
+
+def get_square_root():
+    x1 = entry1.get()
+    
+    label3 = tk.Label(root, text='The Square Root of ' + x1 + ' is:', font=('helvetica', 10))
+    canvas1.create_window(200, 210, window=label3)
+    
+    label4 = tk.Label(root, text=float(x1)**0.5, font=('helvetica', 10, 'bold'))
+    canvas1.create_window(200, 230, window=label4)
+    
+button1 = tk.Button(text='Get the Square Root', command=get_square_root, bg='brown', fg='white', font=('helvetica', 9, 'bold'))
+canvas1.create_window(200, 180, window=button1)
+
+root.mainloop()
